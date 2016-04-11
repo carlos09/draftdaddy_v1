6db7b546bdf243ce23c5d59b63662bfd10d488c6
Carlos <carlos.esquer927@gmail.com>
2016-04-10 23:49:13 -0700

    tracking

--- wp-content/themes/bridge/header.php
+++ wp-content/themes/bridge/header.php
@@ -24,18 +24,17 @@
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<link rel="shortcut icon" type="image/x-icon" href="<?php echo esc_url($qode_options_proya['favicon_image']); ?>">
 	<link rel="apple-touch-icon" href="<?php echo esc_url($qode_options_proya['favicon_image']); ?>"/>
-	<?php wp_head();
-
+	<?php wp_head(); ?>
 	<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-76243235-1', 'auto');
-  ga('send', 'pageview');
+		ga('create', 'UA-76243235-1', 'auto');
+		ga('send', 'pageview');
 
-</script>
+	</script>
 </head>
 
 <body <?php body_class(); ?>>
